f11781441c670fc21ccbf65489681fa104fab940
Dandi Sabilli Izzulhaq <45232854+dandiabil@users.noreply.github.com>
2021-03-20 13:48:54 +0700

    UI v2
    Perubahan tampilan utama dan juga penambahan skrip untuk animasi

--- script.js
+++ script.js
@@ -4,23 +4,54 @@ window.onload = function(){
     logo.classList.remove('fade');
 };
 
+// logo.addEventListener('mouseover', function(){
+    
+// });
 
+//select navbar
+const liNav = document.querySelectorAll('nav ul li');
+liNav.forEach(function(e){
+    e.addEventListener('click', function(){
+        // console.log(e.className);
+        const home = document.querySelector('.home');
+        const profile = document.querySelector('.profile');
+        const work = document.querySelector('.work');
+        const social = document.querySelector('.social');
 
-
-//tombol up
-const parent = document.querySelector('.parent');
-const btnUp = document.querySelector('.btn-up');
-btnUp.addEventListener('click', function(){
-    parent.scrollTop = 0;
+        switch(e.className){
+            case 'profile-menu':
+                if(!home.classList.contains('hidden') || !work.classList.contains('hidden') || !social.classList.contains('hidden')){
+                    profile.classList.remove('hidden');
+                    home.classList.add('hidden');
+                    work.classList.add('hidden');
+                    social.classList.add('hidden');
+                }else{
+                    home.classList.remove('hidden');
+                    profile.classList.add('hidden');
+                }
+                break;
+            case 'work-menu':
+                if(!home.classList.contains('hidden') || !profile.classList.contains('hidden') || !social.classList.contains('hidden')){
+                    work.classList.remove('hidden');
+                    home.classList.add('hidden');
+                    profile.classList.add('hidden');
+                    social.classList.add('hidden');
+                }else{
+                    home.classList.remove('hidden');
+                    work.classList.add('hidden');
+                }
+                break;
+            case 'social-menu':
+                if(!home.classList.contains('hidden') || !work.classList.contains('hidden') || !profile.classList.contains('hidden')){
+                    social.classList.remove('hidden');
+                    home.classList.add('hidden');
+                    work.classList.add('hidden');
+                    profile.classList.add('hidden');
+                }else{
+                    home.classList.remove('hidden');
+                    social.classList.add('hidden');
+                }
+                break;
+        }
+    });
 });
-
-
-parent.addEventListener('scroll', function(){
-    if(parent.scrollTop > 800){
-        btnUp.classList.remove('fade');
-    }else{
-        btnUp.classList.add('fade');
-    }
-});
-
-
